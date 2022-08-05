6d2aa428028f541cdb79fa1e0b913baa0beb0472
XiaoJiang <97390917+xiaojiangclassmate@users.noreply.github.com>
2022-08-05 20:50:40 +0800

    Initial commit

---
+++ editor/src/main/java/com/xiaojiangi/editor/CodeEditor.java
@@ -0,0 +1,42 @@
+package com.xiaojiangi.editor;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CodeEditor extends View {
+    public CodeEditor(Context context) {
+        super(context);
+        init(context);
+
+    }
+
+    public CodeEditor(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+    }
+
+    public CodeEditor(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+
+    }
+
+    public CodeEditor(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context){
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
